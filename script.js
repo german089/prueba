6a9cb30b381587e41e1a8e0dc830efe869976563
german089 <german089@gmail.com>
2023-10-05 01:01:44 -0300

    este es un commit , un mensaje para saber lo que se subio

---
+++ script.js
@@ -0,0 +1,15 @@
+let nombre = "German";
+var apellido =" Aquino";
+var nombreUsuario ="germanito"
+var contraseña = "1234"
+
+let persona = {
+ id : 1,
+ apellido : "gonzalez",
+ nombre : "julio",
+
+
+
+}
+
+console.log(persona)
